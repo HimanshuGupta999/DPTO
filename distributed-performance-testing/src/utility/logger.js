8f555ec203b0270e61331b638a1f6b36bba02ffc
Himanshu <himanshu963954632@gmail.com>
2025-06-24 18:15:43 +0530

    Updated logger file and yml file

--- distributed-performance-testing/src/utility/logger.js
+++ distributed-performance-testing/src/utility/logger.js
@@ -1,28 +1,6 @@
 const winston = require('winston');
 const Transport = require('winston-transport');
 
-// class MetricsTransport extends Transport {
-//     constructor(opts) {
-//         super(opts);
-//         this.fileTransport = new winston.transports.File({
-//             filename: 'remote-metrics.log',
-//             level: 'info',
-//             format: winston.format.combine(
-//                 winston.format.timestamp(),
-//                 winston.format.prettyPrint() // << pretty JSON format
-//             )
-//         });
-//     }
-
-//     log(info, callback) {
-//         if (info.message && typeof info.message === 'object' && info.message.type === 'metrics') {
-//             this.fileTransport.log(info, callback);
-//         } else {
-//             callback();
-//         }
-//     }
-// }
-
 class MetricsTransport extends Transport {
     constructor(opts) {
         super(opts);
@@ -31,24 +9,19 @@ class MetricsTransport extends Transport {
             level: 'info',
             format: winston.format.combine(
                 winston.format.timestamp(),
-                winston.format.prettyPrint()
+                winston.format.prettyPrint() // << pretty JSON format
             )
         });
     }
 
     log(info, callback) {
-    if (info.message && typeof info.message === 'object' && info.message.type === 'metrics') {
-        this.fileTransport.log(info, callback);
-
-        // Also log to console in CI for visibility
-        console.log(`[METRICS] ${info.message.label} - ${info.message.output || ''}`);
-    } else {
-        callback();
+        if (info.message && typeof info.message === 'object' && info.message.type === 'metrics') {
+            this.fileTransport.log(info, callback);
+        } else {
+            callback();
+        }
     }
 }
-
-}
-
 
 const logger = winston.createLogger({
     level: 'info',
