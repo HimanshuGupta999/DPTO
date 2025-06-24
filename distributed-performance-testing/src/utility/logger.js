ccc91c3a2950eda89b41f539bab698f7226743ac
Himanshu <himanshu963954632@gmail.com>
2025-06-24 15:25:28 +0530

    updating index.js file

--- distributed-performance-testing/src/utility/logger.js
+++ distributed-performance-testing/src/utility/logger.js
@@ -1,6 +1,28 @@
 const winston = require('winston');
 const Transport = require('winston-transport');
 
+// class MetricsTransport extends Transport {
+//     constructor(opts) {
+//         super(opts);
+//         this.fileTransport = new winston.transports.File({
+//             filename: 'remote-metrics.log',
+//             level: 'info',
+//             format: winston.format.combine(
+//                 winston.format.timestamp(),
+//                 winston.format.prettyPrint() // << pretty JSON format
+//             )
+//         });
+//     }
+
+//     log(info, callback) {
+//         if (info.message && typeof info.message === 'object' && info.message.type === 'metrics') {
+//             this.fileTransport.log(info, callback);
+//         } else {
+//             callback();
+//         }
+//     }
+// }
+
 class MetricsTransport extends Transport {
     constructor(opts) {
         super(opts);
@@ -9,7 +31,7 @@ class MetricsTransport extends Transport {
             level: 'info',
             format: winston.format.combine(
                 winston.format.timestamp(),
-                winston.format.prettyPrint() // << pretty JSON format
+                winston.format.prettyPrint()
             )
         });
     }
@@ -17,11 +39,17 @@ class MetricsTransport extends Transport {
     log(info, callback) {
         if (info.message && typeof info.message === 'object' && info.message.type === 'metrics') {
             this.fileTransport.log(info, callback);
-        } else {
-            callback();
+
+            // ✅ Also log to console for visibility in CI/CD
+            const label = info.message.label || '';
+            const output = info.message.output || '';
+            const host = info.message.host || '';
+            console.log(`[REMOTE METRICS] ${label} ${host} :: ${output}`);
         }
+        callback();
     }
 }
+
 
 const logger = winston.createLogger({
     level: 'info',
